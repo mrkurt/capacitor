e6da4980019aff892cec777bc65c908a6a94f480
Kurt Mackey <kurt@mubble.net>
2009-07-27 13:26:27 -0500

    Removing validation of relationships on ProjectCategory type

--- app/models/project_category.rb
+++ app/models/project_category.rb
@@ -2,9 +2,6 @@ class ProjectCategory < ActiveRecord::Base
   belongs_to :project
   belongs_to :category
   
-  validates_presence_of :project_id
-  validates_presence_of :category_id
-  
   def name
     category.name
   end
